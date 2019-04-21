7636c20f053b8bf6b5e55582675263693d3f0ac6
Sean Haneberg <haneberg@gmail.com>
2019-04-20 18:13:06 -0700

    Updates

---
+++ precache-manifest.4497d4820df24d166883227f7532b97e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ancient-roman-generator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ba6170674fa221d8cc00",
+    "url": "/ancient-roman-generator/static/js/runtime~main.6fa6b1ad.js"
+  },
+  {
+    "revision": "5800968a06736c5dc794",
+    "url": "/ancient-roman-generator/static/js/main.c9eb09ed.chunk.js"
+  },
+  {
+    "revision": "c22b6e04e5217c6f5e96",
+    "url": "/ancient-roman-generator/static/js/2.a8086230.chunk.js"
+  },
+  {
+    "revision": "5800968a06736c5dc794",
+    "url": "/ancient-roman-generator/static/css/main.7ad41fd1.chunk.css"
+  },
+  {
+    "revision": "e9ba43ee826e9256b729df2755066057",
+    "url": "/ancient-roman-generator/index.html"
+  }
+];
