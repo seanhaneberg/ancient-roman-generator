703429f44cc2bdb8ed1095b35dc9b736dda0f11c
Sean Haneberg <haneberg@gmail.com>
2019-04-20 17:48:50 -0700

    use .jsx and hello world for names

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import NameGenerator from './tools/NameGenerator';
 class App extends Component {
   render() {
+    let string = new NameGenerator().testMessage();
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {string}
           </p>
           <a
             className="App-link"
---
+++ src/tools/NameGenerator.jsx
@@ -0,0 +1,7 @@
+class NameGenerator {
+  testMessage() {
+    return "Hello, World!";
+  }
+}
+
+export default NameGenerator;
