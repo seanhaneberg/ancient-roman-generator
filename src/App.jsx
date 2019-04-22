bfa3398ad29b0b4c9eec6669502c18921d05b253
Sean Haneberg <haneberg@gmail.com>
2019-04-21 23:21:42 -0700

    Styling and invoke from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import NameGenerator from './tools/NameGenerator';
 class App extends Component {
   render() {
-    let string = new NameGenerator().testMessage();
+    let string = new NameGenerator().generateNomen();
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {string}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <p>
+          {string}
+        </p>
       </div>
     );
   }
