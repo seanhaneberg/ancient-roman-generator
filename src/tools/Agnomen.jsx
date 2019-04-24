e6b74f0d2598cd7ef55dc5152e7baf2e5b727e6b
Sean Haneberg <haneberg@gmail.com>
2019-04-23 21:59:32 -0700

    small agnomen file

---
+++ src/tools/Agnomen.jsx
@@ -0,0 +1,28 @@
+/**
+ * Abbreviations
+ * https://en.wikipedia.org/wiki/Praenomen
+ */
+const agnomen = [
+  'cicero',
+  'callidus',
+  'callidus',
+  'campana',
+  'candidus',
+  'canis',
+  'carus',
+  'cernuus',
+  'ceterus',
+  'curtus',
+  'iustus',
+  'maculosus',
+  'paternus',
+  'patronus',
+  'perfectus',
+  'pignus',
+  'plagiarius',
+  'tertius',
+  'virtus',
+  'vulgus'
+];
+
+export default agnomen;
--- src/tools/NameGenerator.jsx
+++ src/tools/NameGenerator.jsx
@@ -5,6 +5,7 @@
 import getAbbrvForName from './Abbrv';
 import cognomina from './Cognomina';
 import { masc_praenomina, femme_praenomina } from './Praenomina';
+import agnomen from './Agnomen';
 
 const selectFrom = (array) => {
   let random = Math.random();
@@ -33,11 +34,25 @@ class NameGenerator {
     return selectFrom(sourceCollection);
   }
 
+  generateAgnomen() {
+    let agnomenChance = !!(Math.random() < 0.5);
+    if (agnomenChance) {
+      let index = Math.floor(agnomen.length * Math.random());
+      // send back a random name
+      let nickname = agnomen[index];
+      let formattedAgnomen = `${nickname.charAt(0).toUpperCase()}${nickname.slice(1)}`
+      return formattedAgnomen
+    } else {
+      return null;
+    }
+  }
+
   generateNomen() {
     let praenomen = this.generatePraenomen();
     let cognomen = this.generateCognomen();
+    let agnomen = this.generateAgnomen();
     let abbr = getAbbrvForName(praenomen);
-    let name = abbr ? `${abbr}. (${praenomen}) ${cognomen}` : `${praenomen} ${cognomen}`;
+    let name = abbr ? `${abbr}. (${praenomen}) ${cognomen} ${agnomen || ''}` : `${praenomen} ${cognomen} ${agnomen || ''}`;
     return name;
   }
 }
