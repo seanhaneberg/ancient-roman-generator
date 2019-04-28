f49fbb3c4dd8e195a22a2c2fda6ed292cb6671cc
Sean Haneberg <haneberg@gmail.com>
2019-04-27 22:08:23 -0700

    Define hills

---
+++ src/tools/Hills.jsx
@@ -0,0 +1,15 @@
+/**
+ * Hills of Rome
+ * https://en.wikipedia.org/wiki/Seven_hills_of_Rome
+ */
+const hills = [
+  'Aventinus',
+  'Caelius',
+  'Palatinus',
+  'Capitolinus',
+  'Esquilinus',
+  'Quirinalis',
+  'Viminalis',
+];
+
+export default hills;
